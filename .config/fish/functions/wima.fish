ebbb63710d4bc8299f6e92c307387e2e860c5755
jimmy savage <jas.savage@gmail.com>
2019-09-14 15:31:50 -0400

    updated

--- .config/fish/functions/wima.fish
+++
@@ -1,3 +0,0 @@
-function wima --description 'alias wima=dig +short myip.opendns.com @resolver1.opendns.com'
-  dig +short myip.opendns.com @resolver1.opendns.com;
-end
---
+++ .config/fish/functions/wip.fish
@@ -0,0 +1,3 @@
+function wip --description 'alias wip=dig +short myip.opendns.com @resolver1.opendns.com'
+  dig +short myip.opendns.com @resolver1.opendns.com;
+end
