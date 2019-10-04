9e2e2f9973eabfe9e5911d212d72ea7627b74898
jsav0 <jas.savage@gmail.com>
2019-10-03 23:37:44 -0400

    cu

---
+++ .config/fish/functions/dfiles.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function dfiles --description 'alias dfiles=cd ~/gits/dotfiles'
+	cd ~/gits/dotfiles $argv;
+end
--- link.sh
+++ link.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
+function help() {
+	echo "try using one of the following or creating a new case:"
+	echo "fish  ,  dunst  ,  bg  ,  bin"
+}
+
 case $1 in
 	fish) rm -rf ~/.config/fish&&ln -s ~/gits/dotfiles/.config/fish ~/.config/;;
+	dunst) rm -rf ~/.config/dunst&&ln -s ~/gits/dotfiles/.config/dunst ~/.config/;;
 
 	bin) rm -rf ~/.local/bin&&ln -s ~/gits/dotfiles/.local/bin ~/.local/;;
 	bg) rm -rf ~/.local/backgrounds&&ln -s ~/gits/dotfiles/.local/backgrounds ~/.local/;;
-	*) echo "error. Try again or setup a new case";;
+	*) help #echo "error. Try again or setup a new case";;
 esac
 
