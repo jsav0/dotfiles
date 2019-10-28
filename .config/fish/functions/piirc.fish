0a0c2f86b70f3b9581113da742034e81fd33309a
jsav0 <jas.savage@gmail.com>
2019-10-28 01:48:54 -0400

    updated

--- .config/fish/functions/piirc.fish
+++ .config/fish/functions/piirc.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function piirc --description 'alias piirc=ssh savage@piirc'
-	ssh savage@piirc $argv;
+function piirc --description 'alias piirc=ssh piirc'
+	ssh piirc $argv;
 end
