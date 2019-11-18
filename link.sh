0103b96b5c32fbc0ba1f26aae08ec2cd7f7ba781
jsavage <jas.savage@gmail.com>
2019-11-17 19:23:28 -0500

    cu

--- link.sh
+++ link.sh
@@ -2,11 +2,12 @@
 
 function help() {
 	echo "try using one of the following or creating a new case:"
-	echo "i3 , fish  ,  dunst  ,  bg  ,  bin"
+	echo "i3 , polybar , misc , sounds , fish  ,  dunst  ,  bg  ,  bin"
 }
 
 case $1 in
 	i3) rm -rf ~/.config/i3&&ln -s ~/gits/dotfiles/.config/i3 ~/.config/;;
+	polybar) rm -rf ~/.config/polybar&&ln -s ~/gits/dotfiles/.config/polybar ~/.config/;;
 	fish) rm -rf ~/.config/fish&&ln -s ~/gits/dotfiles/.config/fish ~/.config/;;
 	misc) rm -rf ~/.config/misc&&ln -s ~/gits/dotfiles/.config/misc ~/.config/;;
 	dunst) rm -rf ~/.config/dunst&&ln -s ~/gits/dotfiles/.config/dunst ~/.config/;;
