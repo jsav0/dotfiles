bcef98fe55cdb60f19b18532bd80b47b701d2f1d
jsav0 <jas.savage@gmail.com>
2020-04-06 07:54:42 -0400

    added delay. and other (temporary) stuff

--- .local/bin/screenshot.sh
+++ .local/bin/screenshot.sh
@@ -8,7 +8,7 @@ prompt() { \
 	FILE=ss_$(date +%F_%H_%M)
 	CHOICE=$(printf "upload to 0x0\nsave\ncopy\nexit" | dmenu -p "Select screenshot type: " | awk '{print $1}')
 	case $CHOICE in
-		save) maim -s > /tmp/$FILE.png && notify-send "screenshot saved to /tmp/ss_$(date +%F_%H_%M).png";;
+		save) maim -s -d 6 > /tmp/$FILE.png && notify-send "screenshot saved to /tmp/ss_$(date +%F_%H_%M).png";;
 		copy) maim -s | xclip -selection clipboard -t image/png;;
 		upload) maim -s > /tmp/$FILE && curl -sF"file=@/tmp/$FILE" https://0x0.st | xclip -selection clipboard && notify-send "uploaded to 0x0";;
 		big) maim > /tmp/$FILE && curl -sF"file=@/tmp/$FILE" https://0x0.st | xclip -selection clipboard && notify-send "uploaded to 0x0";;
@@ -17,6 +17,5 @@ prompt() { \
 }
 
 case "$1" in
-	#*) maim -g 1060x431+1822+337 > /tmp/$FILE.png && notify-send "screenshot saved to /tmp/ss_$(date +%F_%H_%M).png";;
-    *) prompt;;
+	*) prompt;;
 esac
