a0deea4e2bcbadccee41300d8cbabd1c88e64433
jsav0 <jas.savage@gmail.com>
2020-04-06 07:55:05 -0400

    changed wip function to canihazip because dig myip.opendns.com stopped working

--- .local/bin/wip.sh
+++ .local/bin/wip.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
-ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
-notify-send "$ip"
+#!/bin/bash
+#ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
+ip=$(curl -s https://canihazip.com/s)
+#notify-send "$ip"
+ip_info=$(curl -s https://ipinfo.io/$ip/json | jq .ip,.city,.region,.country)
+echo ""
+echo "Public IPv4 Address: $ip"
+echo ""
+echo "$ip_info"
+
