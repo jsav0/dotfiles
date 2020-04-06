301a91c40d6a468dabbdf6650deee9749a34abfa
jsav0 <jas.savage@gmail.com>
2020-04-06 07:54:17 -0400

    removed notification for now

--- .local/bin/ipinfo.sh
+++ .local/bin/ipinfo.sh
@@ -10,6 +10,7 @@ link=$(cat /dev/stdin)
 ip=$(curl -s https://ipinfo.io/$link/json)
 ip_info=$(curl -s https://ipinfo.io/$link/json | jq .ip,.city,.region,.country)
 
-notify-send "$ip_info"
+#notify-send "$ip_info"
+echo "$ip_info"
 
 
