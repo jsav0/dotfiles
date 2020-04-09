ec2c5b605b2b8a3b3304a1bfdfd3817504fb0c9e
jsav0 <jas.savage@gmail.com>
2020-04-09 03:12:07 -0400

    fixed

--- .local/bin/goto_webUI.sh
+++ .local/bin/goto_webUI.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-UI_CHOICES="piirc\npihole\ntherouter\nubmon\noiduabox"
+UI_CHOICES="piirc\npihole\ntherouter\neyez"
 UI=$(printf "$UI_CHOICES" | dmenu -p "Select a webUI:" | awk '{print $1}') 
 case $UI in 
 	pihole) firefox pi.hole/admin;;
 	therouter) firefox 192.168.10.1;;
-	ubmon) firefox ubmon/login;;
-	oiduabox) firefox oiduabox/playback;;
 	piirc) firefox piirc:7272;;
+	eyez) firefox --new-instance -P blank --class=eyez eyez;;
 	*) exit 0;;
 esac
 
