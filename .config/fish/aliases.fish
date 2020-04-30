7d1fc73dacd05e593adeb57d2d6b5cc5ece5a07a
jsav0 <jas.savage@gmail.com>
2020-04-29 21:32:31 -0400

    updated aliases

--- .config/fish/aliases.fish
+++ .config/fish/aliases.fish
@@ -1,25 +1,19 @@
 # general navigation
-alias bgs 'cd ~/.local/backgrounds'
 alias bin 'cd ~/.local/bin'
-alias cfg 'cd ~/.config'
+alias config 'cd ~/.config'
 alias g 'cd ~/gits/'
-
 
 # gopher pastebin
 alias 1337 'nc 0x1A4.1337.cx 9998'
 
-
 # general simplifications
+alias l 'ls'
 alias latr 'ls -latr'
 alias vi 'nvim'
+alias clip 'xclip -selection clipboard'
 
 # herbstclient
 alias hc 'herbstclient'
-
-# xbps-query
-#alias xi 'sudo xbps-install -Su'
-#alias xq 'sudo xbps-query -R'
-#alias xqs 'sudo xbps-query -Rs'
 
 # change git config
 function gconf 
@@ -27,17 +21,24 @@ function gconf
 end
 alias gcm='git commit -m'
 
-
 # get public ip
 function wip
         #dig +short myip.opendns.com @resolver1.opendns.com
 	curl -s https://canihazip.com/s
 end
 
-
 # get info on any public ip 
 function ipi
 	~/.local/bin/ipinfo.sh $argv[1];
 end
 
+# get weather info
+function wx
+	curl wttr.in/Charlotte;
+end
+
+# chmod x
+function x
+	chmod +x $argv[1];
+end
 
